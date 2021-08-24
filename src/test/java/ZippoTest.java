e7cb9667721cc8c68b9a87caa25ae1cfc7442d70
ismettechno <ismettechno@gmail.com>
2021-08-24 20:06:13 +0100

    statusCodeTest eklendi

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -19,4 +19,17 @@ public class ZippoTest {
         ;
     }
 
+    @Test
+    public void statusCodeTest()
+    {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()   //  log.All() -> bütün respons u gösterir
+        ;
+    }
+
+
+
 }
