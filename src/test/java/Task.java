15f7cba4d4f64bef6b821432b815438441af2c87
ismettechno <ismettechno@gmail.com>
2021-08-30 18:33:11 +0100

    task2

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -1,8 +1,9 @@
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
-
+import static org.hamcrest.Matchers.*;
 
 public class Task {
 
@@ -25,6 +26,47 @@ public class Task {
                 .contentType(ContentType.TEXT)
         ;
     }
+
+    /** Task 2
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type TEXT
+     * expect BODY to be equal to "203 Non-Authoritative Information"
+     */
+
+
+    @Test
+    public void task2()
+    {
+        given()
+
+                .when()
+                .get("https://httpstat.us/203")
+
+                .then()
+                .statusCode(203)
+                .contentType(ContentType.TEXT)
+                .body(equalTo("203 Non-Authoritative Information"))
+        ;
+
+        //2.yöntem
+        String bodyText=
+        given()
+
+                .when()
+                .get("https://httpstat.us/203")
+
+                .then()
+                .statusCode(203)
+                .contentType(ContentType.TEXT)
+                .extract().body().asString()
+        ;
+
+        Assert.assertTrue(bodyText.equalsIgnoreCase("203 Non-Authoritative Information"));
+    }
+
+
+
 
 
 
