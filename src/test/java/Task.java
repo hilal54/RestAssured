d2c2a8c7d68df0944602289bfc341897f01adefb
ismettechno <ismettechno@gmail.com>
2021-08-30 19:23:14 +0100

    task3 task 4

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -7,15 +7,15 @@ import static org.hamcrest.Matchers.*;
 
 public class Task {
 
-    /** Task 1
+    /**
+     * Task 1
      * create a request to https://httpstat.us/203
      * expect status 203
      * expect content type TEXT
      */
 
     @Test
-    public void task1()
-    {
+    public void task1() {
         given()
 
                 .when()
@@ -27,7 +27,8 @@ public class Task {
         ;
     }
 
-    /** Task 2
+    /**
+     * Task 2
      * create a request to https://httpstat.us/203
      * expect status 203
      * expect content type TEXT
@@ -36,8 +37,7 @@ public class Task {
 
 
     @Test
-    public void task2()
-    {
+    public void task2() {
         given()
 
                 .when()
@@ -50,24 +50,89 @@ public class Task {
         ;
 
         //2.yöntem
-        String bodyText=
-        given()
+        String bodyText =
+                given()
 
-                .when()
-                .get("https://httpstat.us/203")
+                        .when()
+                        .get("https://httpstat.us/203")
 
-                .then()
-                .statusCode(203)
-                .contentType(ContentType.TEXT)
-                .extract().body().asString()
-        ;
+                        .then()
+                        .statusCode(203)
+                        .contentType(ContentType.TEXT)
+                        .extract().body().asString();
 
         Assert.assertTrue(bodyText.equalsIgnoreCase("203 Non-Authoritative Information"));
     }
 
 
+    /**
+     * Task 3
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
+     */
 
+    @Test
+    public void test3() {
+        given()
 
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
+        ;
+    }
+
+    /**
+     * Task 4
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect response completed status to be false
+     */
+
+    @Test
+    public void task4()
+    {
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("completed",equalTo(false))
+        ;
+
+        //2.yol
+        boolean completed=
+                given()
+                        .when()
+                        .get("https://jsonplaceholder.typicode.com/todos/2")
+                                .then()
+                                .statusCode(200)
+                                .contentType(ContentType.JSON)
+                                // .log().body()
+                                .body("completed",equalTo(false))
+                                .extract().path("completed")
+                ;
+
+        Assert.assertFalse(completed);
+
+    }
 
 
 }
+
+
+
+
+
+
